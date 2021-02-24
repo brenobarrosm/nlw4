8344229a728b7aa419312ddbcce6ad9a59f48654
Breno Barros <brenowbarros21@gmail.com>
2021-02-24 07:52:14 -0400

    Criando controller, model e repository de surveys

--- src/controllers/UserController.ts
+++ src/controllers/UserController.ts
@@ -1,13 +1,14 @@
 import { Request, Response } from 'express';
-import { getRepository } from 'typeorm';
+import { getCustomRepository, getRepository } from 'typeorm';
 import { User } from '../models/User';
+import { UserRepository } from '../repositories/UserRepository';
 
 class UserController {
 
     async create(request: Request, response: Response) {
         const { name, email } = request.body;
 
-        const usersRepository = getRepository(User);
+        const usersRepository = getCustomRepository(UserRepository);
 
         const userAlreadyExists = await usersRepository.findOne({
             email
